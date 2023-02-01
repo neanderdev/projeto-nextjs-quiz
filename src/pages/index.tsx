da7cce16b7be169d016c9aa9a599296eaa90d539
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 16:28:42 -0300

    Componente Botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import Questao from '../components/Questao';
+import Botao from '../components/Botao';
 
 import QuestaoModel from '../model/questao';
 import RespostaModel from '../model/resposta';
@@ -14,18 +15,13 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
 
 export default function Home() {
   const [questao, setQuestao] = useState(questaoMock);
-  const questaoRef = useRef<QuestaoModel>();
-
-  useEffect(() => {
-    questaoRef.current = questao;
-  }, [questao]);
 
   function respostaFornecida(indice: number) {
     setQuestao(questao.responderCom(indice));
   }
 
   function tempoEsgotado() {
-    if (questaoRef.current?.naoRespondida) {
+    if (questao.naoRespondida) {
       setQuestao(questao.responderCom(-1));
     }
   }
@@ -34,6 +30,7 @@ export default function Home() {
     <div
       style={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
@@ -44,6 +41,11 @@ export default function Home() {
         tempoParaResposta={10}
         respostaFornecida={respostaFornecida}
         tempoEsgotado={tempoEsgotado}
+      />
+
+      <Botao
+        texto='Próxima'
+        href='/resultado'
       />
     </div>
   )
