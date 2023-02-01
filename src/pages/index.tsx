8a16fa8cc2d57224fafed011c508992a14638947
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 16:10:50 -0300

    Componente Temporizador

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,12 @@ export default function Home() {
     setQuestao(questao.responderCom(indice));
   }
 
+  function tempoEsgotado() {
+    if (questao.naoRespondida) {
+      setQuestao(questao.responderCom(-1));
+    }
+  }
+
   return (
     <div
       style={{
@@ -31,6 +37,7 @@ export default function Home() {
       <Questao
         valor={questao}
         respostaFornecida={respostaFornecida}
+        tempoEsgotado={tempoEsgotado}
       />
     </div>
   )
