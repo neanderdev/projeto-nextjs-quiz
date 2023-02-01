e5528f208025d70e2ebfbd17c60292b053c844d8
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 12:29:47 -0300

    Convertendo para Objeto

--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import questoes from "../bancoDeQuestoes";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(questoes[0]);
+  res.status(200).json(questoes[0].converterParaObjeto());
 }
