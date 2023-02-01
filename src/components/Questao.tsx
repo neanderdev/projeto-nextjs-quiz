d0cdb98688168d2ad95c8482f00f565ebc5a8757
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 15:11:13 -0300

    Componente Enunciado

--- src/components/Questao.tsx
+++ src/components/Questao.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Questao.module.css';
 
 import QuestaoModel from '../model/questao';
+import Enunciado from './Enunciado';
 
 interface QuestaoProps {
     valor: QuestaoModel,
@@ -11,7 +12,7 @@ export default function Questao(props: QuestaoProps) {
 
     return (
         <div className={styles.questao}>
-            <h1>Questão</h1>
+            <Enunciado texto={questao.enunciado} />
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,17 @@ export default function Home() {
   ]);
 
   return (
-    <Questao
-      valor={questaoTeste}
-    />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Questao
+        valor={questaoTeste}
+      />
+    </div>
   )
 }
