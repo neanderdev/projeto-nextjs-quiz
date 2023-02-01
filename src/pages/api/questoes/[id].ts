2b385a2abdea56cfe837e9ba69415825fd116340
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 14:37:33 -0300

    Construindo a API #01

--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -3,5 +3,17 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import questoes from "../bancoDeQuestoes";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(questoes[0].converterParaObjeto());
+  const idSelecionado = Number(req.query.id);
+
+  const unicaQuestaoOuNada = questoes.filter(
+    (questao) => questao.id === idSelecionado
+  );
+
+  if (unicaQuestaoOuNada.length === 1) {
+    const questaoSelecionada = unicaQuestaoOuNada[0];
+
+    res.status(200).json(questaoSelecionada.converterParaObjeto());
+  } else {
+    res.status(204).send();
+  }
 }
