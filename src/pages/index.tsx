d839fbf8337a1366a3226c2a4a9da0f1ddc57a84
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 15:04:43 -0300

    Iniciando Componente Questão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import Questao from '../components/Questao';
+
+import QuestaoModel from '../model/questao';
+import RespostaModel from '../model/resposta';
+
 export default function Home() {
+  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
+    RespostaModel.errada('Vermelha'),
+    RespostaModel.errada('Verde'),
+    RespostaModel.errada('Laranja'),
+    RespostaModel.certa('Azul'),
+  ]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Questao
+      valor={questaoTeste}
+    />
   )
 }
