b71368983bcc5bebf504b67de8ea69dad50ed0f5
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 15:18:31 -0300

    Componente Resposta #01

--- src/components/Questao.tsx
+++ src/components/Questao.tsx
@@ -1,7 +1,9 @@
 import styles from '../styles/Questao.module.css';
 
 import QuestaoModel from '../model/questao';
+
 import Enunciado from './Enunciado';
+import Resposta from './Resposta';
 
 interface QuestaoProps {
     valor: QuestaoModel,
@@ -10,9 +12,23 @@ interface QuestaoProps {
 export default function Questao(props: QuestaoProps) {
     const questao = props.valor;
 
+    function renderizarRespostas() {
+        return questao.respostas.map((resposta, i) => {
+            return <Resposta
+                key={i.toString()}
+                valor={resposta}
+                indice={i}
+                letra='A'
+                corLetra='#F2C866'
+            />
+        })
+    }
+
     return (
         <div className={styles.questao}>
             <Enunciado texto={questao.enunciado} />
+
+            {renderizarRespostas()}
         </div>
     );
 }
