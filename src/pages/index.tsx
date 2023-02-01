cf1363c1466a29ab5813a21faee77adee7c8a445
Neander de Souza <neander.faria@hotmail.com>
2023-02-01 16:46:42 -0300

    Componente Index #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import Questionario from '../components/Questionario';
 
@@ -12,29 +12,46 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.certa('Azul'),
 ]);
 
+const BASE_URL = 'http://localhost:3000/api';
+
 export default function Home() {
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
   const [questao, setQuestao] = useState(questaoMock);
+
+  async function carregarIdsDasQuestoes() {
+    const resposta = await fetch(`${BASE_URL}/questionario`);
+    const idsDasQuestoes = await resposta.json();
+
+    setIdsDasQuestoes(idsDasQuestoes);
+  }
+
+  const carregarQuestao = useCallback(async (idQuestao: number) => {
+    const resposta = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
+    const json = await resposta.json();
+
+    console.log(json);
+  }, []);
+
+  useEffect(() => {
+    carregarIdsDasQuestoes();
+  }, []);
+
+  useEffect(() => {
+    if (idsDasQuestoes.length > 0) {
+      carregarQuestao(idsDasQuestoes[0]);
+    }
+  }, [idsDasQuestoes, carregarQuestao]);
 
   function questaoRespondida(questao: QuestaoModel) { }
 
   function irPraProximoPasso() { }
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      <Questionario
-        questao={questao}
-        ultima={true}
-        questaoRespondida={questaoRespondida}
-        irPraProximoPasso={irPraProximoPasso}
-      />
-    </div>
+    <Questionario
+      questao={questao}
+      ultima={true}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />
   )
 }
