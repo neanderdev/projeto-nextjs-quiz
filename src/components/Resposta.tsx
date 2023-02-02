2cac94bb5844f570e2c7a294d4d11db360bd41b0
Neander de Souza <neander.faria@hotmail.com>
2023-02-02 10:04:30 -0300

    Animação Resposta

--- src/components/Resposta.tsx
+++ src/components/Resposta.tsx
@@ -12,53 +12,50 @@ interface RespostaProps {
 
 export default function Resposta(props: RespostaProps) {
     const resposta = props.valor;
+    const respostaReveçada = resposta.revelada ? styles.respostaRevelada : '';
 
     return (
-        <div className={styles.resposta} onClick={() => props.respostaFornecida(props.indice)}>
-            <div className={styles.conteudoResposta}>
-                {
-                    !resposta.revelada
-                        ? (
-                            <div className={styles.frente}>
-                                <div
-                                    className={styles.letra}
-                                    style={{ backgroundColor: props.corFundoLetra }}
-                                >
-                                    {props.letra}
+        <div
+            className={styles.resposta}
+            onClick={() => props.respostaFornecida(props.indice)}
+        >
+            <div className={`${styles.conteudoResposta} ${respostaReveçada}`}>
+                <div className={styles.frente}>
+                    <div
+                        className={styles.letra}
+                        style={{ backgroundColor: props.corFundoLetra }}
+                    >
+                        {props.letra}
+                    </div>
+
+                    <div className={styles.valor}>
+                        {resposta.valor}
+                    </div>
+                </div>
+
+                < div className={styles.verso}>
+                    {
+                        resposta.certa
+                            ? (
+                                <div className={styles.certa}>
+                                    <div>A resposta certa é...</div>
+
+                                    <div className={styles.valor}>
+                                        {resposta.valor}
+                                    </div>
                                 </div>
+                            )
+                            : (
+                                <div className={styles.errada}>
+                                    <div>A resposta informada está errada...</div>
 
-                                <div className={styles.valor}>
-                                    {resposta.valor}
+                                    <div className={styles.valor}>
+                                        {resposta.valor}
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                        : (
-                            < div className={styles.verso}>
-                                {
-                                    resposta.certa
-                                        ? (
-                                            <div className={styles.certa}>
-                                                <div>A resposta certa é...</div>
-
-                                                <div className={styles.valor}>
-                                                    {resposta.valor}
-                                                </div>
-                                            </div>
-                                        )
-                                        : (
-                                            <div className={styles.errada}>
-                                                <div>A resposta informada está errada...</div>
-
-                                                <div className={styles.valor}>
-                                                    {resposta.valor}
-                                                </div>
-                                            </div>
-                                        )
-                                }
-                            </div>
-
-                        )
-                }
+                            )
+                    }
+                </div>
             </div>
         </div >
     );
