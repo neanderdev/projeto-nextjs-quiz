9bf21c4c63afa1998aeee64f59c34edede96790b
Neander de Souza <neander.faria@hotmail.com>
2023-02-02 09:42:31 -0300

    Componente Resultado

--- src/pages/resultado.tsx
+++ src/pages/resultado.tsx
@@ -1,7 +1,23 @@
+import { useRouter } from 'next/router';
+
+import styles from '../styles/Resultado.module.css';
+
 export default function Resultado() {
+    const router = useRouter();
+
+    const total = Number(router.query.total);
+    const certas = Number(router.query.certas);
+    const percentual = Math.round((certas / total) * 100);
+
     return (
         <div>
-            <h1>Resultado</h1>
+            <h1>Resultado Final</h1>
+
+            <div>{total}</div>
+
+            <div>{certas}</div>
+
+            <div>{`${percentual}%`}</div>
         </div>
     );
 }
